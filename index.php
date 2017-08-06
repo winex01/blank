2469d1603578c329f5a6e9969fc4e5fc11c9326e
winnie131212592 <winnie131212592@gmail.com>
2017-08-06 18:01:15 +0800

    added the authlogin

--- index.php
+++ index.php
@@ -6,3 +6,21 @@
 # TODO task 3
 # TODO task 4
 # TODO task 5
+
+/**
+ *
+ */
+class Login
+{
+
+  function __construct()
+  {
+    # code...
+    // this is the default constructor test
+  }
+
+  function validate($username, $password)
+  {
+    #do some stuff here
+  }
+}
